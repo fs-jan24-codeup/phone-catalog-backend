0438dab334bf5455dab74ecabef38182a80521e8
Tetiana Lisne <tplovic@gmail.com>
2024-05-21 21:35:35 +0300

    test husky

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.json({ test: 'Ok' });
 });
 
