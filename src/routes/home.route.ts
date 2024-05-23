ae0562aef80076a1f772b4c9e72e9088642be245
illya-onyshchuk <llayonys@gmail.com>
2024-05-23 19:43:28 +0300

    remove unnecessary file

--- src/routes/home.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-// import { getAll } from '../controllers/product.controller';
-
-const router = express.Router();
-
-// router.get('/home', getAll);
-
-export const productRouterApp = router;
