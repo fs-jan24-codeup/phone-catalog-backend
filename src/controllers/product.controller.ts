84f353185c4bea67898739b5647de337fcf23116
illya-onyshchuk <llayonys@gmail.com>
2024-05-23 16:02:55 +0300

    uncomment changes

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,56 +1,56 @@
-// import { Status } from '../types/constants';
-// import * as productService from '../services/product.service';
-// import { Request, Response } from 'express';
-// import { Product } from '../types/types';
+import { Status } from '../types/constants';
+import * as productService from '../services/product.service';
+import { Request, Response } from 'express';
+import { Product } from '../types/types';
 
-// export const getAll = async (req: Request, res: Response) => {
-//   const products = await productService.getAll();
-//   res.send(products.map((prod: Product) => productService.normalize(prod)));
-// };
+export const getAll = async (req: Request, res: Response) => {
+  const products = await productService.getAll();
+  res.send(products.map((prod: Product) => productService.normalize(prod)));
+};
 
-// export const getOne = async (req: Request, res: Response) => {
-//   const { id } = req.params;
-//   const product = await productService.getOne(id);
+export const getOne = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await productService.getOne(id);
 
-//   if (!product) {
-//     return res.sendStatus(404);
-//   }
+  if (!product) {
+    return res.sendStatus(404);
+  }
 
-//   res.send(productService.normalize(product));
-// };
+  res.send(productService.normalize(product));
+};
 
-// export const create = async (req: Request, res: Response) => {
-//   const { name } = req.body;
+export const create = async (req: Request, res: Response) => {
+  const { name } = req.body;
 
-//   if (!name) {
-//     return res.sendStatus(400);
-//   }
+  if (!name) {
+    return res.sendStatus(400);
+  }
 
-//   const product = await productService.createOne({ name });
-//   res.status(Status.CREATED).send(productService.normalize(product));
-// };
+  const product = await productService.createOne({ name });
+  res.status(Status.CREATED).send(productService.normalize(product));
+};
 
-// export const update = async (req: Request, res: Response) => {
-//   const { id } = req.params;
-//   const { name } = req.body;
+export const update = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name } = req.body;
 
-//   const product = await productService.updateOne(id, { name });
-//   res.status(Status.OK).send(productService.normalize(product));
-// };
+  const product = await productService.updateOne(id, { name });
+  res.status(Status.OK).send(productService.normalize(product));
+};
 
-// export const remove = async (req: Request, res: Response) => {
-//   const { id } = req.params;
-//   const product = await productService.getOne(id);
+export const remove = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await productService.getOne(id);
 
-//   if (!product) {
-//     return res.sendStatus(Status.NOT_FOUND);
-//   }
+  if (!product) {
+    return res.sendStatus(Status.NOT_FOUND);
+  }
 
-//   const deleted = await productService.deleteOne(id);
+  const deleted = await productService.deleteOne(id);
 
-//   if (!deleted) {
-//     return res.sendStatus(Status.NOT_FOUND);
-//   }
+  if (!deleted) {
+    return res.sendStatus(Status.NOT_FOUND);
+  }
 
-//   res.sendStatus(Status.NO_CONTENT);
-// };
+  res.sendStatus(Status.NO_CONTENT);
+};
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,42 +1,45 @@
-// import { PrismaProduct } from '../models/Product.model';
-// import { Product } from '../types/types.js';
+import { PrismaProduct } from '../models/Product.model';
+import { Product } from '../types/types.js';
 
-// const normalize = ({ id, name }: Product): Product => {
-//   return { id, name };
-// };
+const normalize = ({ id, name }: Product): Product => {
+  return { id, name };
+};
 
-// async function getAll(): Promise<Product[]> {
-//   const result = await PrismaProduct.findMany();
+async function getAll(): Promise<Product[]> {
+  const result = await PrismaProduct.findMany();
 
-//   return result;
-// }
+  return result;
+}
 
-// async function getOne(id: number): Promise<Product | null> {
-//   return PrismaProduct.findByPk(id);
-// }
+async function getOne(id: number): Promise<Product | null> {
+  return PrismaProduct.findByPk(id);
+}
 
-// async function createOne({ id, name }: Product): Promise<Product> {
-//   return PrismaProduct.create({ id, name });
-// }
+async function createOne({ id, name }: Product): Promise<Product> {
+  return PrismaProduct.create({ id, name });
+}
 
-// async function updateOne(id: number, { name }: Partial<Product>): Promise<Product | null> {
-//   await PrismaProduct.update({ name }, { where: { id } });
+async function updateOne(
+  id: number,
+  { name }: Partial<Product>,
+): Promise<Product | null> {
+  await PrismaProduct.update({ name }, { where: { id } });
 
-//   return getOne(id);
-// }
+  return getOne(id);
+}
 
-// async function deleteOne(id: number): Promise<boolean> {
-//   await PrismaProduct.destroy({ where: { id } });
+async function deleteOne(id: number): Promise<boolean> {
+  await PrismaProduct.destroy({ where: { id } });
 
-//   return true;
-// }
+  return true;
+}
 
-// export {
-//   normalize,
-//   PrismaProduct,
-//   getAll,
-//   getOne,
-//   createOne,
-//   updateOne,
-//   deleteOne,
-// };
+export {
+  normalize,
+  PrismaProduct,
+  getAll,
+  getOne,
+  createOne,
+  updateOne,
+  deleteOne,
+};
